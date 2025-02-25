62584eac8344abf45ac76be6ae44d4dcad4e127c
Suelen Medina <suelenmedinape@gmail.com>
2025-02-24 21:07:22 -0300

    :arrow_up: Merge

--- admin-app/src/app/autentication/service/produto/produto.service.ts
+++ admin-app/src/app/autentication/service/produto/produto.service.ts
@@ -13,7 +13,7 @@ export class ProdutoService {
   constructor(private http: HttpClient, private cookieService: CookieService) {}
  
   getProdutos(): Observable<any> {
-    return this.http.get(this.apiUrl); 
+    return this.http.get(`${this.apiUrl}/info-product-admin`); 
   }
 
   addProduct(product: any): Observable<any> {
@@ -21,5 +21,27 @@ export class ProdutoService {
     const headers = new HttpHeaders().set("Authorization", `Bearer ${token}`);
     console.log("Token no addProduct:", token);
     return this.http.post(this.apiUrl, product, {headers});
+  }
+
+  getProdutoByName(name: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/search?name=${name}`);
+  }
+
+  deleteProduct(id: number): Observable<any> {
+    const token = this.cookieService.get('auth_token');
+    const headers = new HttpHeaders().set("Authorization", `Bearer ${token}`);
+    console.log("Token no addProduct:", token);
+    return this.http.delete(`${this.apiUrl}/${id}`, {headers});
+  }
+
+  editProduct(product: any): Observable<any> {
+    const token = this.cookieService.get('auth_token');
+    const headers = new HttpHeaders().set("Authorization", `Bearer ${token}`);
+    console.log("Token no addProduct:", token);
+    return this.http.put(`${this.apiUrl}/${product.id}`, product, {headers});
+  }
+
+  getProductById(id: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`);
   }
 }
--- admin-app/src/app/pages/home/home.component.ts
+++ admin-app/src/app/pages/home/home.component.ts
@@ -2,12 +2,15 @@ import { Component, HostListener, OnInit } from '@angular/core';
 import { ProdutoService } from '../../autentication/service/produto/produto.service';
 import { PaginationComponent } from "../../shared/_component/pagination/pagination.component";
 import { CurrencyPipe } from '@angular/common';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, Subject } from 'rxjs';
+import { AlertComponent } from '../../shared/models/alert/alert.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [PaginationComponent, CurrencyPipe, RouterLink, RouterLinkActive],
+  imports: [PaginationComponent, AlertComponent, CurrencyPipe, RouterLink, RouterLinkActive, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -19,12 +22,33 @@ export class HomeComponent implements OnInit {
   currentPage = 1; // Página inicial
   isMenuOpen = false;
 
+  showAlert: boolean = false;
+  message: string = "";
+  categAlert: number = 0;
+
+  searchTerm: string = '';
+  isSearching: boolean = false;
+  private searchSubject = new Subject<string>();
+
   constructor(
-    private produtosService: ProdutoService
+    private produtosService: ProdutoService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     this.listProducts();
+
+    // Configura busca com debounce
+    this.searchSubject.pipe(
+      debounceTime(300), // Aguarda 300ms antes de buscar
+      distinctUntilChanged() // Evita buscas duplicadas
+    ).subscribe(term => {
+      if (term.trim() === '') {
+        this.listProducts(); // Se estiver vazio, carrega tudo novamente
+      } else {
+        this.getByNameProduct(term);
+      }
+    });
   }
 
   // Total de páginas calculado com base no número de produtos
@@ -58,12 +82,57 @@ export class HomeComponent implements OnInit {
   listProducts(): void {
       this.produtosService.getProdutos().subscribe({
         next: (dados) => {
-          
           this.produtos = dados;
         },
         error: (err) => {
           console.error('Erro ao buscar produtos:', err);
         },
       });
+  }
+
+  buscarProdutos(): void {
+    this.searchSubject.next(this.searchTerm); // Envia o termo para ser processado
+  }
+
+  getByNameProduct(nome: string): void {
+    this.isSearching = true;
+    this.produtosService.getProdutoByName(nome).subscribe(
+      produtos => {
+        this.produtos = produtos;
+        this.showAlert = produtos.length === 0;
+        this.isSearching = false;
+
+        if (this.showAlert) {
+          setTimeout(() => this.showAlert = false, 3000);
+        }
+      },
+      error => {
+        this.showAlert = true;
+        this.message = "Erro ao buscar produtos.";
+        this.categAlert = 2;
+        this.isSearching = false;
+      }
+    );
+  }
+
+  deleteProduct(id: number): void {
+    this.produtosService.deleteProduct(id).subscribe(
+      () => {
+        this.listProducts();
+        this.showAlert = true;
+        this.message = "Produto deletado com sucesso.";
+        this.categAlert = 3;
+
+      },
+      error => {
+        this.showAlert = true;
+        this.message = "Erro ao deletar produto.";
+        this.categAlert = 2;
+      }
+    );
+  }
+
+  editProduct(id: number): void {
+    this.router.navigate(['/edit', id]);
   }
 }
