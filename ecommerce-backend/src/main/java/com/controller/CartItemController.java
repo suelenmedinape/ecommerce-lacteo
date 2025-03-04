18895f5f4a4d6cc07f42c6e6bd0a5e0a1567a572
Jhonata Castro <166658525+JhonnyBCastro@users.noreply.github.com>
2025-03-04 12:12:11 -0300

    refactor: modificar chamada no metodo UpdateItemQuantity

--- ecommerce-backend/src/main/java/com/controller/CartItemController.java
+++ ecommerce-backend/src/main/java/com/controller/CartItemController.java
@@ -64,10 +64,10 @@ public class CartItemController {
 	}
 
 	@PutMapping("/update")
-	public ResponseEntity<?> updateItemQuantity(@RequestBody CartItemDTO cartItemDTO) {
+	public ResponseEntity<Void> updateItemQuantity(@RequestBody CartItemDTO cartItemDTO) {
 		String email = SecurityContextHolder.getContext().getAuthentication().getName();
 		Client client = clientService.findByEmail(email);
-		cartService.updateItemFromCart(client.getId(), cartItemDTO.getProductId(), cartItemDTO.getQuantity());
+		cartService.addItemToCart(client.getId(), cartItemDTO.getProductId(), cartItemDTO.getQuantity());
 
 		return ResponseEntity.ok(Collections.singletonMap("message", "Quantidade atualizada com sucesso"));
 	}
